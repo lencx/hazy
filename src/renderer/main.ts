70139a38a020a3164bf775b3f4fe4629cb8d2fbd
Len <cxin1314@gmail.com>
2018-05-28 22:52:03 +0800

    langs & axios

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
 import './core/ui'
@@ -7,6 +8,7 @@ import router from './core/router'
 import store from './core/store'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
     router,
